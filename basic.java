64b5b65e95e0cc8a93561e8705e466de65dc4adb
ombodkhe2021 <obodkhe2021@gmail.com>
2026-01-03 09:06:52 +0000

    operators

--- basic.java
+++ basic.java
@@ -27,17 +27,82 @@ class basic{
        System.out.println("Multi of a and b are :" +multi);
        int div = a/b;
        System.out.println("division of a and b are :" +div);
-*/
+Q4
        System.out.print("Enter the length : ");
        int l = sc.nextInt();
        System.out.print("Enter the breadth : ");
        int b = sc.nextInt();
        int area = l*b;
        System.out.println(" The area of rectangle :"  +area );
-
-
-
+ Q5
+    System.out.println("Enter the radius of Circle : ");
+       int r = sc.nextInt();
+       int diameter = 2 * r;
+       System.out.println("the diameter of the circle is :"  +diameter);
+       float circumference = 2*3.14f*r*r;
+       System.out.println("The circumference of the circle is :" +circumference);
+       float area = 3.14f *r*r;
+       System.out.println("the area of the circle is"  +area);
+       Q6
+       System.out.print("Enter the number in centimeter : ");
+       long cm = sc.nextLong();
+       double m = cm*0.01D;
+       System.out.println("The number in meter is : " +m);
+       double km = cm*0.001D;
+       System.out.println("The number in kilometer = " +km);
+       Q7
        
+      System.out.print("Enter the number in fah ");
+      double fah = sc.nextDouble();
+      double cel = (fah - 32 ) *5/9;
+      System.out.println("The number in cel is " +cel);
+       Q8
+      System.out.print("Enter the numbers in cel : ");
+        double cel = sc.nextDouble();
+        double fah = (cel * 9/5) +32;
+        System.out.println(" The number in fah is " +fah);
+         Q9
+        System.out.println("Enter the 1st angle of the triangle in degree : ");
+        int ang1 = sc.nextInt();
+        System.out.println("Enter the 2ne angle of the triangle in degree :  ");
+        int ang2 = sc.nextInt();
+        int ang3 =  180-(ang1+ang2);
+        System.out.println("The third angle is :" +ang3);
+         Q10 
+        System.out.print("Enter the the length equal side of a triangle : ");
+        long a = sc.nextLong();
+        double area = (1.73D/2) *a;
+        System.out.println("The are of the triangle is : " +area);  
+        Q11
+        System.out.print("Enter the mark of subject 1 out of 100 : ");
+        int sub1 = sc.nextInt();
+        System.out.print("Enter the mark of subject 2 out of 100 : ");
+        int sub2 = sc.nextInt();
+        System.out.print("Enter the mark of subject 3 out of 100 : ");
+        int sub3 = sc.nextInt();
+        System.out.print("Enter the mark of subject 4 out of 100 : ");
+        int sub4 = sc.nextInt();
+        System.out.print("Enter the mark of subject 5 out of 100 : ");
+        int sub5 = sc.nextInt();
+        double total = sub1+sub2+sub3+sub4+sub5;
+        System.out.println(" Total marks obtain are : " +total);
+        double percentage = (total/500)*100;
+        System.out.println("The percentage scored is :" +percentage +"%"); 
+        System.out.print("Enter the principal amount : ");
+        Q12
+        long p = sc.nextLong();
+        System.out.print("Enter the Rate of interest in percentage  : ");
+        long r = sc.nextLong();
+        System.out.println("%");
+        System.out.print("Enter the time : ");
+        long t = sc.nextLong();
+        System.out.println("year");
+        double si = (p*r*t)/100;
+        System.out.println(" The Simple Interest is : " +si);
+        Q13*/
+        
+        
+
 
     }
 }
